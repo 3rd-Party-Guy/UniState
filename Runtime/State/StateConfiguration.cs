678b6038c16370d4a81d149a463becc87e3b371f
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-02 02:51:41 +0100

    use unihelper

--- Runtime/State/StateConfiguration.cs
+++ Runtime/State/StateConfiguration.cs
@@ -1,23 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UniHelper;
 
 namespace UniState {
     public class StateConfiguration<TState, TTrigger> {
         public IEnumerable<Func<object>> EntryFunctions => onEntryFunctions;
         public IEnumerable<Func<object>> ExitFunctions => onExitFunctions;
         
-        public IReadOnlyDictionary<TState, IEnumerable<Func<object>>> OnEntryFromFunctions =>
-            onEntryFromFunctions.ToDictionary(
-                kvp => kvp.Key,
-                kvp => kvp.Value.AsReadOnly() as IEnumerable<Func<object>>
-            );
-
-        public IReadOnlyDictionary<TState, IEnumerable<Func<object>>> OnExitToFunctions =>
-            onExitToFunctions.ToDictionary(
-                kvp => kvp.Key,
-                kvp => kvp.Value.AsReadOnly() as IEnumerable<Func<object>>
-            );
+        public IReadOnlyDictionary<TState, IEnumerable<Func<object>>> OnEntryFromFunctions => onEntryFromFunctions.ReadOnlyDictionary;
+        public IReadOnlyDictionary<TState, IEnumerable<Func<object>>> OnExitToFunctions => onExitToFunctions.ReadOnlyDictionary;
 
         public IReadOnlyDictionary<TTrigger, TState> DefinedTransitions => definedTransitions;
         public IEnumerable<TTrigger> IgnoredTriggers => ignoredTriggers;
@@ -25,8 +17,8 @@ namespace UniState {
         readonly List<Func<object>> onEntryFunctions = new();
         readonly List<Func<object>> onExitFunctions = new();
 
-        readonly Dictionary<TState, List<Func<object>>> onEntryFromFunctions = new();
-        readonly Dictionary<TState, List<Func<object>>> onExitToFunctions = new();
+        readonly ObservableListDictionary<TState, Func<object>> onEntryFromFunctions = new();
+        readonly ObservableListDictionary<TState, Func<object>> onExitToFunctions = new();
 
         readonly Dictionary<TTrigger, TState> definedTransitions = new();
         readonly List<TTrigger> ignoredTriggers = new();

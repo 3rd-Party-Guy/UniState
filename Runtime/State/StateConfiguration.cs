5668f8127c9994c7b2e62df5516dbae8ed20f2ac
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-02 01:40:18 +0100

    remove random import

--- Runtime/State/StateConfiguration.cs
+++ Runtime/State/StateConfiguration.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.ShaderKeywordFilter;
 
 namespace UniState {
     public class StateConfiguration<TState, TTrigger> {

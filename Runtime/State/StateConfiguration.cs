7fbc62f49319e9cd53942a429dcdcd8bdebd296c
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-02 01:37:58 +0100

    add specific transitions

--- Runtime/State/StateConfiguration.cs
+++ Runtime/State/StateConfiguration.cs
@@ -1,16 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.ShaderKeywordFilter;
 
 namespace UniState {
     public class StateConfiguration<TState, TTrigger> {
         public IEnumerable<Func<object>> EntryFunctions => onEntryFunctions;
         public IEnumerable<Func<object>> ExitFunctions => onExitFunctions;
         
+        public IReadOnlyDictionary<TState, IEnumerable<Func<object>>> OnEntryFromFunctions =>
+            onEntryFromFunctions.ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value.AsReadOnly() as IEnumerable<Func<object>>
+            );
+
+        public IReadOnlyDictionary<TState, IEnumerable<Func<object>>> OnExitToFunctions =>
+            onExitToFunctions.ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value.AsReadOnly() as IEnumerable<Func<object>>
+            );
+
         public IReadOnlyDictionary<TTrigger, TState> DefinedTransitions => definedTransitions;
         public IEnumerable<TTrigger> IgnoredTriggers => ignoredTriggers;
 
         readonly List<Func<object>> onEntryFunctions = new();
         readonly List<Func<object>> onExitFunctions = new();
+
+        readonly Dictionary<TState, List<Func<object>>> onEntryFromFunctions = new();
+        readonly Dictionary<TState, List<Func<object>>> onExitToFunctions = new();
 
         readonly Dictionary<TTrigger, TState> definedTransitions = new();
         readonly List<TTrigger> ignoredTriggers = new();
@@ -32,6 +49,18 @@ namespace UniState {
 
         public StateConfiguration<TState, TTrigger> OnExit(Func<object> action) {
             onExitFunctions.Add(action);
+            return this;
+        }
+
+        public StateConfiguration<TState, TTrigger> OnEntryFrom(TState state, Func<object> action) {
+            onEntryFromFunctions[state] ??= new();
+            onEntryFromFunctions[state].Add(action);
+            return this;
+        }
+
+        public StateConfiguration<TState, TTrigger> OnExitTo(TState state, Func<object> action) {
+            onExitToFunctions[state] ??= new();
+            onExitToFunctions[state].Add(action);
             return this;
         }
     }

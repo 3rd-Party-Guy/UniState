aa00af48425cdb4573688bbea2a76aedc2f48009
Nikolay Hadzhiev <nikolayhadzhiev@outlook.com>
2025-03-02 01:05:51 +0100

    modification of configuration

--- Runtime/State/StateConfiguration.cs
+++ Runtime/State/StateConfiguration.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 
 namespace UniState {
     public class StateConfiguration<TState, TTrigger> {
-        List<Func<object>> OnEntryFunctions = new();
-        List<Func<object>> OnExitFunctions = new();
+        readonly List<Func<object>> onEntryFunctions = new();
+        readonly List<Func<object>> onExitFunctions = new();
 
-        Dictionary<TTrigger, TState> DefinedTransitions = new();
-        
+        readonly Dictionary<TTrigger, TState> definedTransitions = new();
+        readonly List<TTrigger> ignoredTriggers = new();
+
+        public StateConfiguration<TState, TTrigger> Permit(TTrigger trigger, TState state) {
+            definedTransitions[trigger] = state;
+            return this;
+        }
+
+        public StateConfiguration<TState, TTrigger> Ignore(TTrigger trigger) {
+            ignoredTriggers.Add(trigger);
+            return this;
+        }
+
+        public StateConfiguration<TState, TTrigger> OnEntry(Func<object> action) {
+            onEntryFunctions.Add(action);
+            return this;
+        }
+
+        public StateConfiguration<TState, TTrigger> OnExit(Func<object> action) {
+            onExitFunctions.Add(action);
+            return this;
+        }
     }
 }
